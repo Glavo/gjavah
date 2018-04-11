5873deb7af12aa2b84221e9e12e0dc7ca5807f0a
Glavo <zjx001202@126.com>
2018-04-11 14:38:54 +0800

    update

---
+++ src/test/java/org/glavo/javah/HeaderGeneratorTest.java
@@ -0,0 +1,14 @@
+package org.glavo.javah;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HeaderGeneratorTest {
+    public static void main(String[] args) throws IOException {
+        PrintWriter writer = new PrintWriter(System.out);
+        HeaderGenerator.generateHeader("java.lang.System", writer);
+        writer.flush();
+    }
+}
